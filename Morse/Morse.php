930a0158f90ad205ba308bd9e51b83932ee4f715
Elisei <mailatelisei27@gmail.com>
2025-10-06 10:43:19 +0200

    Added arrow icons to button and position fixed

--- Morse/Morse.php
+++ Morse/Morse.php
@@ -117,18 +117,18 @@
                         </div>
 
 
-                        <div class="col">
-                            <div class="row btn btn-primary mb-3">
-                                <button type="submit" name="toMorse" class="btn">
-                                    testo -> morse
-                                </button>
-                            </div>
+                        <div class="d-flex justify-content-center gap-3 mb-3">
+                            <button type="submit" name="toMorse" class="btn btn-primary px-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
+                                </svg>
+                            </button>
 
-                            <div class="row btn btn-primary mb-3">
-                                <button type="submit" name="toText" class="btn">
-                                    morse -> testo
-                                </button>
-                            </div>
+                            <button type="submit" name="toText" class="btn btn-secondary px-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
+                                </svg>
+                            </button>
                         </div>
 
 
