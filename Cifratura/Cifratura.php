0a209b0cbe4aa6a33cd930b1d4b65661a5cd7afc
Elisei <mailatelisei27@gmail.com>
2025-10-06 12:23:36 +0200

    Added Cifratura project

--- Cifratura/Cifratura.php
+++ Cifratura/Cifratura.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Morse</title>
+    <title>Cifratura di Cesare</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
@@ -13,93 +13,12 @@
 
 <?php
 
-    $alfabeto =
-            array(
-                'a' => '.-',
-                'b' => '-...',
-                'c' => '-.-.',
-                'd' => '-..',
-                'e' => '.',
-                'f' => '..-.',
-                'g' => '--.',
-                'h' => '....',
-                'i' => '..',
-                'j' => '.---',
-                'k' => '-.-',
-                'l' => '.-..',
-                'm' => '--',
-                'n' => '-.',
-                'o' => '---',
-                'p' => '.--.',
-                'q' => '--.-',
-                'r' => '.-.',
-                's' => '...',
-                't' => '-',
-                'u' => '..-',
-                'v' => '...-',
-                'w' => '.--',
-                'x' => '-..-',
-                'y' => '-.--',
-                'z' => '--..',
-                '1' => '.----',
-                '2' => '..---',
-                '3' => '...--',
-                '4' => '....-',
-                '5' => '.....',
-                '6' => '-....',
-                '7' => '--...',
-                '8' => '---..',
-                '9' => '----.',
-                '10' => '-----',
-                ' ' => '|',
 
-            );
-
-    if(!isset($_POST["text"]))
+    if(!isset($_POST['text']))
         $text = "";
 
-    if(!isset($_POST["morse"]))
-        $morse = "";
-
-
-
-    if(isset($_POST["text"]) && isset($_POST["toMorse"])){
-
-        $text = strtolower($_POST["text"]);
-        $morse = "";
-
-        for ($i = 0; $i < strlen($text); $i++) {
-            $char = $text[$i];
-            if (isset($alfabeto[$char])) {
-                $morse .= $alfabeto[$char] . " ";
-            } else {
-                $morse .= "? ";
-            }
-        }
-
-
-    }
-    else if (isset($_POST["morse"]) && isset($_POST["toText"])){
-
-        $reverse = array_flip($alfabeto);
-        $morse = trim($_POST["morse"]);
-
-        $simboli = explode(" ", $morse);
-        $text = "";
-
-        for ($i = 0; $i < count($simboli); $i++) {
-            $segno = trim($simboli[$i]);
-
-            if ($segno == "|" || $segno == "/") {
-                $text .= " ";
-            } else if (isset($reverse[$segno])) {
-                $text .= $reverse[$segno];
-            } else if ($segno != "") {
-                $text .= "?";
-            }
-        }
-
-    }
+    if(!isset($_POST['cifratura']))
+        $cifratura = "";
 
 ?>
 
@@ -114,12 +33,12 @@
 
                     <!-- Header -->
                     <div id="header" class="text-center mb-2">
-                        <h1 class="fw-bold">Morse</h1>
+                        <h1 class="fw-bold">Cifratura di Cesare</h1>
                         <p class="text-muted">Traduttore da testo in morse e viceversa</p>
                     </div>
 
                     <!-- Form -->
-                    <form action="Morse.php" method="post">
+                    <form action="Cifratura.php" method="post">
 
                         <!-- Input -->
                         <div class="mb-3">
@@ -135,6 +54,10 @@
                                 </svg>
                             </button>
 
+                            <input type="text" name="offset" class="form-control "style="max-width: 50px;">
+                                <text></text>
+                            </input>
+
                             <button type="submit" name="toText" class="btn btn-secondary px-4">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
@@ -144,8 +67,8 @@
 
 
                         <div class="mb-3">
-                            <textarea type="text" class="form-control text-box" name="morse"
-                                   placeholder="Morse"><?php echo $morse;?></textarea>
+                            <textarea type="text" class="form-control text-box" name="cifratura"
+                                   placeholder="Morse"><?php echo $cifratura;?></textarea>
                         </div>
 
 
