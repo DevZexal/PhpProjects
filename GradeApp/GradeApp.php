7e6953b91ee3559dfd310670856fa4768a7499a3
Elisei <mailatelisei27@gmail.com>
2025-10-08 08:03:24 +0200

    Added GradeApp project

---
+++ GradeApp/GradeApp.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GradeApp</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+
+</head>
+<body class="bg-black">
+
+<?php
+
+
+?>
+
+<div id="app">
+
+
+
+</div>
+
+</body>
+</html>
