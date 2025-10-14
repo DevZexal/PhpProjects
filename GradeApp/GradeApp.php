3d7a2319148e45a15a1ae65e018477201dfe256e
Elisei <mailatelisei27@gmail.com>
2025-10-14 18:13:34 +0200

    Added GradeApp

--- GradeApp/GradeApp.php
+++ GradeApp/GradeApp.php
@@ -9,16 +9,80 @@
     <link href="style.css" rel="stylesheet">
 
 </head>
-<body class="bg-black">
+<body class="bg-white">
+
+<div class="container mt-5">
+
+<h1 class="mb-4">Calcola la media voti</h1>
+
+<!-- FORM FILTRI -->
+<form method="post" class="row g-3 mb-4" action="GradeApp.php">
+    <div class="col-md-4">
+        <label for="cognome" class="form-label">Cognome</label>
+        <input type="text" name="cognome" id="cognome" class="form-control"
+               placeholder="Inserisci il cognome"
+               value="<?php echo isset($_GET['cognome']) ? $_GET['cognome'] : ''; ?>">
+    </div>
+
+    <div class="col-md-4">
+        <label for="classe" class="form-label">Classe</label>
+        <input type="text" name="classe" id="classe" class="form-control"
+               placeholder="Es. 3BIT"
+               value="<?php echo isset($_GET['classe']) ? $_GET['classe'] : ''; ?>">
+    </div>
+
+    <div class="col-md-4">
+        <label for="materia" class="form-label">Materia</label>
+        <input type="text" name="materia" id="materia" class="form-control"
+               placeholder="Es. Matematica"
+               value="<?php echo isset($_GET['materia']) ? $_GET['materia'] : ''; ?>">
+    </div>
+
+    <div class="col-12 text-end">
+        <button type="submit" class="btn btn-primary">Calcola media</button>
+        <a href="GradeApp.php" class="btn btn-secondary">Reset</a>
+    </div>
+</form>
 
 <?php
 
+    function calcolaMedia($cognome = null, $classe = null, $materia = null){
+        $somma = 0;
+        $count = 0;
+        $media = 0;
+
+        $file = file("random-grades.csv");
+        for($i = 1; $i < count($file); $i++){
+            $riga = explode(",", $file[$i]);
+
+            if(isset($cognome) && $cognome != $riga[0]) continue;
+            if(isset($classe) && $classe != $riga[2]) continue;
+            if(isset($materia) && $materia != $riga[3]) continue;
+
+            $somma += $riga[5];
+            $count++;
+        }
+
+        if($count > 0){
+            $media = $somma / $count;
+
+            echo "La media";
+            echo isset($cognome) ? " di $cognome" : "";
+            echo isset($classe) ? " di Classe $classe" : "";
+            echo isset($materia) ? " di $materia" : "";
+            echo " e': " . $media;
+        }
+
+    }
+
+    $cognome = isset($_POST["cognome"]) ? $_POST["cognome"] : null;
+    $classe  = isset($_POST["classe"])  ? $_POST["classe"]  : null;
+    $materia = isset($_POST["materia"]) ? $_POST["materia"] : null;
+
+
+    calcolaMedia($cognome, $classe, $materia);
 
 ?>
-
-<div id="app">
-
-
 
 </div>
 
