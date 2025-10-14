0f3e8428f75b742fc8f9d78943c3b0138858d280
Elisei <mailatelisei27@gmail.com>
2025-10-14 23:58:46 +0200

    Removed style.css
    Don't used

--- GradeApp/GradeApp.php
+++ GradeApp/GradeApp.php
@@ -6,7 +6,6 @@
     <title>GradeApp</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
 
 </head>
 <body class="bg-white">
