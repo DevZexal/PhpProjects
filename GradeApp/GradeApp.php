ff057c1772cde24593b7c28abf0ea58e677b7a93
Elisei <mailatelisei27@gmail.com>
2025-10-21 23:53:54 +0200

    Added 2nd point

--- GradeApp/GradeApp.php
+++ GradeApp/GradeApp.php
@@ -51,7 +51,6 @@
 
     function calcolaMedie($cognome = null, $classe = null, $materia = null){
         $file = file("random-grades.csv");
-        $header = str_getcsv($file[0], ",", '"', "\\");
 
         $voti = [];
 
@@ -73,9 +72,12 @@
         return $voti;
     }
 
+
     $cognome = $_POST["cognome"] ?? null;
     $classe  = $_POST["classe"] ?? null;
     $materia = $_POST["materia"] ?? null;
+
+    $voti = [];
 
     if($cognome || $classe || $materia){
         $voti = calcolaMedie($cognome, $classe, $materia);
@@ -83,31 +85,78 @@
         if(empty($voti)){
             echo "<div class='alert alert-warning'>Nessun dato trovato.</div>";
         } else {
-            foreach($voti as $studente => $materie){
-                echo "<h3 class='mt-4'>Studente: <strong>$studente</strong></h3>";
 
-                $sommaGenerale = 0;
-                $countGenerale = 0;
+            if($classe && !$cognome){
+                echo "<h3 class='mt-4'>Tabella classe: <strong>$classe</strong></h3>";
 
-                echo "<table class='table table-striped'>";
-                echo "<thead><tr><th>Materia</th><th>Media</th></tr></thead><tbody>";
+                // Trova tutte le materie presenti nella classe
+                $tutteMaterie = [];
+                foreach($voti as $studente => $materie){
+                    foreach(array_keys($materie) as $m){
+                        $tutteMaterie[$m] = true;
+                    }
+                }
+                $tutteMaterie = array_keys($tutteMaterie);
 
-                foreach($materie as $nomeMateria => $votiMateria){
-                    $mediaMateria = array_sum($votiMateria) / count($votiMateria);
-                    $sommaGenerale += array_sum($votiMateria);
-                    $countGenerale += count($votiMateria);
+                echo "<table class='table table-bordered text-center'>";
+                echo "<thead><tr><th>Studente</th>";
 
-                    echo "<tr><td>$nomeMateria</td><td>" . number_format($mediaMateria, 2) . "</td></tr>";
+                foreach($tutteMaterie as $m){
+                    echo "<th>$m</th>";
+                }
+                echo "<th>Media Generale</th></tr></thead><tbody>";
+
+                // Righe studenti
+                foreach($voti as $studente => $materie){
+                    echo "<tr><td><strong>$studente</strong></td>";
+
+                    $somma = 0; $count = 0;
+                    foreach($tutteMaterie as $m){
+                        if(isset($materie[$m])){
+                            $mediaMateria = array_sum($materie[$m]) / count($materie[$m]);
+                            echo "<td>" . number_format($mediaMateria, 2) . "</td>";
+                            $somma += array_sum($materie[$m]);
+                            $count += count($materie[$m]);
+                        } else {
+                            echo "<td>-</td>";
+                        }
+                    }
+
+                    $mediaGenerale = $count ? $somma / $count : 0;
+                    echo "<td><strong>" . number_format($mediaGenerale, 2) . "</strong></td></tr>";
                 }
 
                 echo "</tbody></table>";
+            }
 
-                $mediaGenerale = $sommaGenerale / $countGenerale;
-                echo "<p><strong>Media generale:</strong> " . number_format($mediaGenerale, 2) . "</p>";
+            else {
+                foreach($voti as $studente => $materie){
+                    echo "<h3 class='mt-4'>Studente: <strong>$studente</strong></h3>";
+
+                    $sommaGenerale = 0;
+                    $countGenerale = 0;
+
+                    echo "<table class='table table-striped'>";
+                    echo "<thead><tr><th>Materia</th><th>Media</th></tr></thead><tbody>";
+
+                    foreach($materie as $nomeMateria => $votiMateria){
+                        $mediaMateria = array_sum($votiMateria) / count($votiMateria);
+                        $sommaGenerale += array_sum($votiMateria);
+                        $countGenerale += count($votiMateria);
+
+                        echo "<tr><td>$nomeMateria</td><td>" . number_format($mediaMateria, 2) . "</td></tr>";
+                    }
+
+                    echo "</tbody></table>";
+
+                    $mediaGenerale = $sommaGenerale / $countGenerale;
+                    echo "<p><strong>Media generale:</strong> " . number_format($mediaGenerale, 2) . "</p>";
+                }
             }
         }
     }
     ?>
+
 
 
 </div>
