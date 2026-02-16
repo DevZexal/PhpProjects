4086f8bb0167e162c15324a93a7da8ed1273fe08
Elisei <mailatelisei27@gmail.com>
2026-02-16 22:39:09 +0100

    Fixed first query

--- QuerySQL/main.php
+++ QuerySQL/main.php
@@ -13,27 +13,30 @@
         exit();
     }
 
+    # variabili
+    $autoreXXX = "Dante";
+
     # Query 1
+
+    echo "<h2>Query 1</h2>";
 
     $sql = "select DISTINCT g.nome as NomeGenere, a.nome as NomeAutore, a.cognome as CognomeAutore 
         from autore a
         join libro l on a.ID = l.id_autore
         join librogenere lg on l.ISBN = lg.idLibro
         join genere g on lg.idGenere = g.ID
-        where a.nome = 'Dante';";
+        WHERE a.nome = '$autoreXXX'";
 
-    $result = $cn->query($sql);
+    $res = $cn->query($sql);
 
-    $row = $result->fetch_array();
-
-    echo "I generi associati ai libri scritti dall'autore " . $row['NomeAutore'] . " " . $row['CognomeAutore']. ' sono:';
-    while($row != null){
-        echo '<br>';
-        echo $row['NomeGenere'];
-        $row = $result->fetch_array();
+    if ($res && $res->num_rows > 0) {
+        $row = $res->fetch_assoc();
+        printText("Generi dei libri scritti da {$row['NomeAutore']} {$row['CognomeAutore']}:");
+        do {
+            printText(" - " . $row['NomeGenere']);
+        } while ($row = $res->fetch_assoc());
     }
-
-    $result->free_result();
+    $res->free_result();
 
     # Query 2
     
